8a2c71caec44defe8d4745f89697618322ce7588
sfragas <93608697+sfragas@users.noreply.github.com>
2023-03-27 11:00:49 +0200

    Update prueba_git.R

--- prueba_git.R
+++ prueba_git.R
@@ -1,5 +1,5 @@
 #Esto es una prueba para trabajar con GIT
 #Añado una nueva linea de códigp
 #Metemos una tercera linea
-#Añadimos uncuarte linea de código
+#Añadimos una cuarta linea de código
 #Añadimos una 5 linea de código desde Github
