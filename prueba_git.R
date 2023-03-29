26667a8368991887753a991ced00b3c48c9c6f37
sfragas <sfragas72@outlook.com>
2023-03-29 16:24:57 +0200

    Vovemos a la primera version del archivo prueba_git.R

--- prueba_git.R
+++ prueba_git.R
@@ -1,6 +1,1 @@
 #Esto es una prueba para trabajar con GIT
-#Añado una nueva linea de códigp
-#Metemos una tercera linea
-#Añadimos una cuarta linea de código
-#Añadimos una 5 linea de código desde Github
-
